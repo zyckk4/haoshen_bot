91c5f3d6ea4e4c67715c978c61595e23c8aa55d0
zyckk4 <2240486388@qq.com>
2022-10-31 09:45:30 +0800

    chore: 增加type hint,格式调整

--- plugins/toolbox.py
+++ plugins/toolbox.py
@@ -5,7 +5,8 @@
 import random
 import os
 from datetime import datetime
-from mirai import At, Dice, Face, Image
+from mirai import At, Dice, Face, Image, GroupMessage, MessageEvent
+from mirai.models import NudgeEvent, MemberJoinEvent
 from utils.utils import Listen, my_filter, send, send_nudge, respond_nudge,\
     mute, Config
 
@@ -16,14 +17,14 @@ plugin = Listen(
 
 
 @plugin.all_mesg()
-async def give_time(event):
+async def give_time(event: MessageEvent):
     '''报时'''
     if str(event.message_chain) == '报时':
         await send(event, f"当前时间：{datetime.now().strftime('%X')}")
 
 
 @plugin.nudge()
-async def on_nudge_message(event):
+async def on_nudge_message(event: NudgeEvent):
     '''戳一戳互动'''
     if event.subject.kind == 'Group' and event.subject.id in Config.get()['nudge_banned_group']:
         return
@@ -39,23 +40,23 @@ async def on_nudge_message(event):
 
 
 @plugin.all_mesg()
-async def play_dice(event):
+async def play_dice(event: MessageEvent):
     '''丢骰子'''
     if "丢色子" == str(event.message_chain) or "丢骰子" == str(event.message_chain):
         ran = random.randint(1, 6)
         await send(event, Dice(ran))
     if str(event.message_chain).startswith('丢'):
         x = str(event.message_chain).replace('丢', '', 1)
-        enum = ['1', '2', '3', '4', '5', '6', '一', '二', '三', '四', '五', '六', '壹',
-                '贰', '叁', '肆', '伍', '陆', 'one', 'two', 'three', 'four', 'five', 'six']
-        for i in range(24):
+        enum = ('1', '2', '3', '4', '5', '6', '一', '二', '三', '四', '五', '六', '壹',
+                '贰', '叁', '肆', '伍', '陆', 'one', 'two', 'three', 'four', 'five', 'six')
+        for i in range(len(enum)):
             if x == enum[i]:
-                await send(event, Dice(i % 6+1))
+                await send(event, Dice(i % 6 + 1))
                 return
 
 
 @plugin.member_join()
-async def welcome_new_member(event):
+async def welcome_new_member(event: MemberJoinEvent):
     '''欢迎新成员'''
     await send(event.member, [At(event.member.id), ",热烈欢迎 "+event.member.member_name+" 加入本群！"]
                + [Face(face_id=99)]*3)
@@ -63,15 +64,15 @@ async def welcome_new_member(event):
 
 
 @plugin.group()
-async def interact_with_owner(event):
+async def interact_with_owner(event: GroupMessage):
     '''和主人互动'''
     if At(Config.bot_qq()) in event.message_chain and event.sender.id == Config.bot_owner_qq():
-        reply = ["是的主人", "好的主人"]
+        reply = ("是的主人", "好的主人")
         await send(event, random.choice(reply), True)
 
 
 @plugin.group()
-async def hit_xiaobing(event):
+async def hit_xiaobing(event: GroupMessage):
     '''揍小冰'''
     if str(event.message_chain) == '揍小冰':
         try:
@@ -83,7 +84,7 @@ async def hit_xiaobing(event):
 
 
 @plugin.group()
-async def pin_picture(event):
+async def pin_picture(event: GroupMessage):
     '''pin图功能'''
     path = f'data/pin_image/pin{event.group.id}'
     lst_path = [path+'.png', path+'.jpg', path+'.jpeg', path+'.gif']
@@ -111,12 +112,14 @@ async def pin_picture(event):
             lst_pic = event.message_chain.get(Image)
         await lst_pic[0].download(lst_path[0])
         await send(event, 'pin图片成功!')
+
     elif str(event.message_chain) == '看pin':
         for p in lst_path:
             if os.path.exists(p):
                 await send(event, Image(path=p), True)
                 return
         await send(event, ["本群当前没有pin的图片", Face(face_id=22)], True)
+
     elif str(event.message_chain) == '删pin':
         for p in lst_path:
             if os.path.exists(p):
