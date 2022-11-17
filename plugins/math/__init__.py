f2811ee09b2d280dc33835cfc93d4e24819cd7f0
zyckk4 <2240486388@qq.com>
2022-11-17 10:47:43 +0800

    fix: 删除无用import,增加一处超时提示

--- plugins/math/__init__.py
+++ plugins/math/__init__.py
@@ -2,14 +2,12 @@
 """
 @author: zyckk4  https://github.com/zyckk4
 """
-import os
 import importlib
 from asyncio import exceptions as e
 from urllib.request import quote
 import aiohttp
-from mirai import Face, Image, MessageEvent
+from mirai import Face, MessageEvent
 from .get_latex import get_latex
-from .poly.sos_manager import SOS_Manager
 from utils.utils import Listen, send, Config
 
 importlib.import_module('plugins.math.ETC_search')
@@ -48,13 +46,15 @@ async def get_wa(event: MessageEvent):
     if str(event.message_chain).startswith('/wa'):
         x = str(event.message_chain).replace('/wa', '', 1).strip()
         url = f"https://api.wolframalpha.com/v1/simple?i={quote(x)}&appid={Config.get()['wa_appid']}"
-        timeout = aiohttp.ClientTimeout(total=20)
-        async with aiohttp.ClientSession(timeout=timeout) as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    img = await resp.read()
-                    await send(event, [], img_bytes=img)
-                else:
-                    await send(event, await resp.text())
-
-
+        timeout = aiohttp.ClientTimeout(total=30)
+        try:
+            async with aiohttp.ClientSession(timeout=timeout) as session:
+                async with session.get(url) as resp:
+                    if resp.status == 200:
+                        img = await resp.read()
+                        await send(event, [], img_bytes=img)
+                    else:
+                        await send(event, await resp.text())
+        except e.TimeoutError:
+            await send(event, ["连接超时", Face(face_id=18)], True)
+            return
