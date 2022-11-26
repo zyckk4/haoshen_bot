ae487604acab61d57b3a60746cacacb821b595d6
zyckk4 <2240486388@qq.com>
2022-11-26 12:33:46 +0800

    style: 空格

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from utils.bot_core import Core
 from utils.instance import core_instance
 
 if __name__ == '__main__':
-    core=Core()
+    core = Core()
     core_instance.set(core)
     core.import_modules()
     core.bot.run()
