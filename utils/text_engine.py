6ad6a5a9bdde54722d1b383c4eed08f96111e926
zyckk4 <zyckk4@qq.com>
2024-03-13 19:34:22 +0800

    feat: 重写完善文转图函数

--- utils/text_engine.py
+++ utils/text_engine.py
@@ -6,17 +6,38 @@
 from PIL import Image, ImageDraw, ImageFont
 
 
-def text_to_img(text: str, font_path: str = "statics/fonts/msyh.ttc", font_size: int = 36):
-    lst = text.split('\n')
-    font = ImageFont.truetype(font_path, font_size)
-    x_max, h_max = 0, 0
-    for t in lst:
-        size_x, size_y = font.getsize(t)
-        x_max = max(x_max, size_x)
-        h_max = max(h_max, size_y)
-    img = Image.new('RGB', (x_max+30, h_max*len(lst)), (255, 255, 255))
-    draw = ImageDraw.Draw(img)
-    for i in range(len(lst)):
-        draw.text((0+2, i*h_max+2), lst[i], fill=0, font=font)
+def text_to_img(text: str,
+                font_path: str = './statics/fonts/msyh.ttc',
+                font_size: int = 18,
+                top_margin: int = 10,
+                bottom_margin: int = 10,
+                left_margin: int = 10,
+                right_margin: int = 10,
+                line_spacing: int = 0,
+                font_color=(0, 0, 0),
+                bg_color=(255, 255, 255)
+                ) -> Image:
+    """将文字转为PIL图片"""
+    lines = text.split('\n')
+    font = ImageFont.truetype(
+        font_path, font_size) if font_path else ImageFont.load_default()
 
-    return img
+    max_width, max_height = 0, 0
+    for line in lines:
+        width, height = font.getbbox(line)[2], font.getbbox(line)[3]
+        max_width = max(max_width, width)
+        max_height = max(max_height, height)
+
+    image = Image.new("RGB",
+                      (max_width + left_margin + right_margin,
+                       (max_height + line_spacing)*len(lines) +
+                       top_margin + bottom_margin - line_spacing),
+                      bg_color)
+    draw = ImageDraw.Draw(image)
+
+    y_text = top_margin
+    for line in lines:
+        draw.text((left_margin, y_text), line, fill=font_color, font=font)
+        y_text += max_height + line_spacing
+
+    return image
